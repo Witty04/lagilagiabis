2559e450cd2e1ca6b6577161b0adb196cc8efc4d
salmann29 <257006111031@student.unsil.ac.id>
2026-04-23 19:59:42 +0700

    ngoding mamen

--- halaman-3/script.js
+++ halaman-3/script.js
@@ -21,7 +21,7 @@
 
     // ===== NAV LOGO → HOME =====
     document.getElementById('navLogo').addEventListener('click', () => {
-      window.location.href = '../halaman 1/indexfix.html';
+      window.location.href = '../index.html';
     });
 
      // ===== NAV LOGO → HOME =====
@@ -36,7 +36,7 @@
 
     // ===== TOMBOL MULAI ANALISIS =====
     document.getElementById('btnMulaiAnalisis').addEventListener('click', () => {
-      window.location.href = '../halaman 2/analisisfix.html';
+      window.location.href = '../halaman-2/analisisfix.html';
     });
 
     // ===== SCROLL REVEAL ANIMATION =====
